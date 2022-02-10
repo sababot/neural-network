a8ae204b6f640bdd05bc187d7d43482b3776bb7d
sababot <twinsgardos@gmail.com>
2022-02-10 22:46:16 +0100

    neural-network header

--- include/neural_network.h
+++ include/neural_network.h
@@ -1,1 +1,32 @@
 #pragma once
+
+#include <iostream>
+#include <vector>
+#include <eigen3/Eigen/Eigen>
+
+typedef float Scalar;
+typedef Eigen::MatrixXF Matrix;
+typedef Eigen::RowVectorXF RowVector;
+typedef Eigen::VectorXF ColVector;
+
+class NeuralNetwork
+{
+public:
+	NeuralNetwork(std::vector<uint> topology, Scalar learningRate = Scalar(0.005));
+
+	void propagateForward(RowVector& input);
+
+	void propagateBackward(RowVector& output);
+
+	void calcErrors(RowVector& output);
+
+	void updateWeights();
+
+	void train(std::vector<RowVector*> data);
+
+	std::vector<RowVector*> neuronLayers;
+	std::vector<RowVector*> cacheLayers;
+	std::vector<RowVector*> deltas;
+	std::vector<Matrix*> weights;
+	Scalar learningRate;
+};
