fac5108e2f38c2ba19e097ec4353b6773ebbf90b
sababot <twinsgardos@gmail.com>
2022-02-14 11:50:40 +0100

    neural network constructor

--- src/neural_network.cpp
+++ src/neural_network.cpp
@@ -1,1 +1,45 @@
 #include "../include/neural_network.h"
+
+// Constructor
+NeuralNetwork::NeuralNetwork(std::vector<uint> topology, Scalar learningRate)
+{
+	this->topology = topology;
+	this->learningRate = learningRate;
+	
+	for (uint i = 0; i < topology.size(); i++)
+	{
+		if (i == topology.size() - 1)
+		{
+			neuronLayers.push_back(new RowVector(topology[i]));
+		}
+		else
+		{
+			neuronLayers.puch_back(new RowVector(topology[i] + 1));
+		}
+
+		cacheLayers.push_back(new RowVector(neuronLayers.size()));
+		deltas.push_back(new RowVector(neuronLayers.size()));
+
+		if (i != topology.size() - 1)
+		{
+			neuronLayers.back()->coeffRef(topology[i]) = 1.0;
+			cacheLayers.back()->coeffRef(topology[i]) = 1.0;
+		}
+
+		if (i > 0)
+		{
+			if (i != topology.size() - 1)
+			{
+				weights.push_back(new Matrix(topology[i - 1] + 1, topology[i] + 1));
+				weights.back()->setRandom();
+				weights.back()->col(topology[i]).setZero();
+				weights.back()->coeffRef(topology[i - 1], topology[i]) = 1.0;
+			}
+			else
+			{
+				weights.push_back(new Matrix(topology[i - 1] + 1, topology[i]));
+				weights.back()->setRandom();
+			}
+		}
+	}
+}
