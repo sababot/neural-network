60a4d7006cd19f82407f97a7b3a43a9659233d0f
sababot <twinsgardos@gmail.com>
2022-04-03 22:49:55 +0200

    neuralnet layer

--- main.cpp
+++ main.cpp
@@ -5,11 +5,18 @@ using namespace std;
 
 int main()
 {
-	Eigen::VectorXd inputs(3);
-	inputs << 1.1, 5, 4.7;
-	Eigen::VectorXd weights(3);
-	weights << 0.5, 1.2, 2.1;
-	double bias = 2.5;
+	Eigen::VectorXd inputs1(3);
+	inputs1 << 1.1, 5, 4.7;
+	Eigen::VectorXd weights1(3);
+	weights1 << 0.5, 1.2, 2.1;
+	double bias1 = 2.5;
 
-	cout << calc_output(inputs, weights, bias) << " = output" << endl;
+	Eigen::VectorXd inputs2(3);
+	inputs2 << 1.1, 5, 4.7;
+	Eigen::VectorXd weights2(3);
+	weights2 << 0.5, 1.2, 2.1;
+	double bias2 = 2.5;
+
+	cout << calc_output(inputs1, weights1, bias1) << " = output" << endl;
+	cout << calc_output(inputs2, weights2, bias2) << " = output" << endl;
 }
