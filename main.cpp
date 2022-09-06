d85a2f633bfa8a2150f4c2f96c17ebcd80648673
sababot <twinsgardos@gmail.com>
2022-09-06 21:37:42 +0200

    test input

--- main.cpp
+++ main.cpp
@@ -11,19 +11,20 @@ int main()
 	// INPUTS
 	Eigen::MatrixXd X = read_data("datasets/spiral/data.csv", 300, 2);
 
+	Eigen::MatrixXd X_test(2, 2);
+	X_test << 2.3, 4.2,
+	          1.5, 3.2;
+
 	/* LAYERS */
 	DenseLayer layer1(2, 3);
 	DenseLayer layer2(3, 3);
 
-	layer1.forward(X);
+	layer1.forward(X_test);
 	layer1.outputs = ActivationReLU(layer1.outputs);
 
 	layer2.forward(layer1.outputs);
 	layer2.outputs = ActivationSoftmax(layer2.outputs);
 
-	layer3.forward(layer2.outputs);
-	layer3.outputs = ActivationSoftmax(layer3.outputs);
-
 	// OUTPUT
-	cout << layer3.outputs << endl;
+	cout << layer2.outputs << endl;
 }
